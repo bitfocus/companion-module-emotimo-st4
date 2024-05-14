450a4bc5f9b717fde9e9b5be6256129a51dca071
Jacob <jacobw@emotimo.com>
2024-05-14 14:46:29 -0700

    Updated the Models Array to add SA2.6 Name

--- models.js
+++ models.js
@@ -3,7 +3,7 @@ module.exports = {
 	MODELS: [
 		{ id: 'Spectrum ST4', series: 'ST4', label: 'Spectrum ST4' },
 		{ id: 'Spectrum ST4.3', series: 'ST4.3', label: 'Spectrum ST4.3' },
-		{ id: 'SA2.6', series: 'SA2.6', label: 'SA2.6' },
+		{ id: 'SA2.6 Conductor', series: 'SA2.6', label: 'SA2.6 Conductor' },
 		{ id: 'Other', series: 'Other', label: 'Other Cameras' }
 	]
 }
